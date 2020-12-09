e71f9febc50e796362942efc663bcaf8361a40e8
Work <dubemizuus@gmail.com>
2020-12-09 05:20:18 +0100

    installled pug

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    // Pug Loader
+    config.module
+      .rule("pug")
+      .test(/\.pug$/)
+      .use("pug-plain-loader")
+      .loader("pug-plain-loader")
+      .end();
+  }
 };
